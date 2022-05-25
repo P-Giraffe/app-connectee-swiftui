a45bde84832112737d054e3e202a68388697d6e7
Cityhunter <johan.guenaoui@gmail.com>
2022-05-25 19:04:28 +0200

    Initial Commit

---
+++ MyFirstChat/MyFirstChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstChatApp.swift
+//  MyFirstChat
+//
+//  Created by Johan Guenaoui on 25/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
